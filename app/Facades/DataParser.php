0deccbb9684ab504a516f8f6f542338963f3cdb7
Faisal Khan <mahboobshah.faisal@gmail.com>
2023-12-13 18:28:31 +0100

    fix typo

--- app/Facades/DataParser.php
+++ app/Facades/DataParser.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static parseData(string $file_path) parses the data
  * @method static discoverColumns(mixed $file, bool $firstRowIsHeader = true) returns the columns
- * @method static createTable(string $table, array $columns, bool $timestamps = false) create table
+ * @method static createTable(string $table, array $columns, bool $timestamps = false) creates table
  * @method static insertData(mixed $file, string $table, bool $firstRowIsHeader = true) inserts the records from parsed file into database
  */
 class DataParser extends Facade
