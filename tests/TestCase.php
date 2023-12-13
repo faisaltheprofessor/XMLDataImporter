2adc03ab7f2f961867f608c47b1da441c642ad87
Faisal Khan <mahboobshah.faisal@gmail.com>
2023-12-13 15:45:52 +0100

    remove sqlite db and create it on the runtime

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,34 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        $this->createSqliteTestingDatabase();
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->removeSqliteTestingDatabase();
+        parent::tearDown();
+    }
+
+    protected function createSqliteTestingDatabase(): void
+    {
+        $filePath = __DIR__.'/Files/testing_db.sqlite';
+
+        if (!file_exists($filePath)) {
+            touch($filePath);
+        }
+    }
+
+    protected function removeSqliteTestingDatabase(): void
+    {
+        $filePath = __DIR__.'/Files/testing_db.sqlite';
+
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+    }
 }
